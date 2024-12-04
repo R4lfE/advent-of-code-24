4b8baf4cddb768a541af82d7571583f021322870
Ralf Eemers <ralf-eemers@hotmail.com>
2024-12-04 12:00:40 +0100

    template update

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -1,8 +1,17 @@
+fn read_input(input: &str) -> Vec<Vec<char>> {
+    input
+        .lines()
+        .map(|line| line.trim().chars().collect())
+        .collect()
+}
+
 pub fn part1(input: &str) -> usize {
+    let input = read_input(input);
     todo!()
 }
 
 pub fn part2(input: &str) -> usize {
+    let input = read_input(input);
     todo!()
 }
 
@@ -18,5 +27,19 @@ mod tests {
         dbg!(part1(&input));
         dbg!(part2(&input));
         Ok(())
+    }
+
+    fn get_input<'a>() -> &'a str {
+        ""
+    }
+
+    #[test]
+    fn part1_test() {
+        assert_eq!(part1(get_input()), 0);
+    }
+
+    #[test]
+    fn part2_test() {
+        assert_eq!(part2(get_input()), 0);
     }
 }
